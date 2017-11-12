cc55e1f402a778848247e802a3cdfe4d74ed1c8c
Omar Zapata <Omar.Zapata@cern.ch>
2017-11-12 12:48:41 +0100

    added new requirements, - Support to load root files from a directory given a path - added category or alias while is reading de files. - added support to apply cuts given an alias - added support to add weigths

--- Dracarys/interface/Plotter.h
+++ Dracarys/interface/Plotter.h
@@ -4,6 +4,7 @@
 #ifndef PLOTTER_H
 #define PLOTTER_H
 
+//ROOT Libraries
 #include<TObject.h>
 #include<TH1F.h>
 #include<TCanvas.h>
@@ -11,32 +12,62 @@
 #include<TString.h>
 #include<TFile.h>
 #include<THStack.h>
+#include<TLegend.h>
+#include<TCut.h>
+
+//STD Libraries
 #include<map>
 #include<iostream>
 
 class Plotter:public TObject
 {
-protected:
-    std::vector<TChain*>  fChains;        //Global chain to handle files and trees
-    std::map<TString,THStack*> fHStacks;  //stack to plot multiple histograms
-    std::vector<TString>  fBranches;      //branches to be plotted
+public:
+    std::map<std::string,TChain*>  fChains;   //Global chain to handle files and trees
+    std::map<std::string,std::pair<THStack*,TLegend*> > fHStacks;  //stack to plot multiple histograms
+    std::vector<std::string>  fBranches;      //branches to be plotted
     UInt_t fNBins;                        //number of bins in histograms
     Double_t fXmin;                       //low value for histograms
     Double_t fXmax;                       //high value for histograms
     TFile    *fOutput;                    //!file to save the plots
     TCanvas  *fCanvas;                    //Global canvas to draw
+    std::string  fTreeName;               //Global Tree name
+    std::map<std::string,TCut>  fCuts;     //Cuts to apply  
 public:
-    Plotter(std::vector<TString> treenames,const std::vector<TString> branches, UInt_t bins=100, Double_t xmin=-10.0, Double_t xmax=10.0);
+    Plotter(std::string treename,const std::vector<std::string> branches, UInt_t bins=100, Double_t xmin=-10.0, Double_t xmax=10.0);
     //copy constructor
     Plotter(const Plotter &p);
     ~Plotter();
 
-    void AddFile(const char *filename, const char *treename="",Long64_t nentries=TTree::kMaxEntries);
-    void AddFriend(TTree *chainOrTree,const char *alias = "");
+    /**
+     * Method to add root files to a TChain given a path.
+     * \param path directory to search root files
+     * \param alias category of the trees in the root files (Signal, Bgk0, Bgk1...)
+     * \param weight weight to apply to all trees in the chain.
+     */
+    void AddDirectory(const Char_t *path,const Char_t *alias,Double_t weight=1);
     
-    std::map<TString,THStack*> &GetPlots();
+    /**
+     * Method to add a single root file to a TChain given a path and alias.
+     * \param alias category of the trees in the root files (Signal, Bgk0, Bgk1...)
+     * \param filename directory to root file
+     * \param weight weight to apply to all trees in the chain.
+     */
+    void AddFile(const Char_t *alias,const Char_t *filename, Double_t weight,Long64_t nentries=TTree::kMaxEntries);
     
+    /**
+     * Method to apply a Cuts to TChain while the histograms are created.
+     * \param alias category of the trees in the root files (Signal, Bgk0, Bgk1...) (TChain)
+     * \param TCut object with the cuts https://root.cern.ch/doc/master/classTCut.html NOTE: mutiples cuts can be addressed in TCut object
+     */
+    void SetCut(const Char_t *alias, TCut cut);
+    
+    std::map<std::string,std::pair<THStack*,TLegend*> > &GetPlots();
+
     void Print();
+    
+public:
+    std::pair<THStack*,TLegend*> &GetPlot(const Char_t *branch);
+    std::vector<std::string> Find(std::string path, std::string pattern="*.root");
     ClassDef(Plotter,0);
 };
 
--- Dracarys/src/Plotter.cxx
+++ Dracarys/src/Plotter.cxx
@@ -1,27 +1,11 @@
 #include<Plotter.h>
 #include<TROOT.h>
-
+#include<TSystem.h>
 static UInt_t counter=0;
 
 //______________________________________________________________________________
-Plotter::Plotter(std::vector<TString> treenames,const std::vector<TString> branches, UInt_t bins,Double_t xmin, Double_t xmax):fBranches(branches),fNBins(bins),fXmin(xmin),fXmax(xmax),fOutput(nullptr)
-{
-    TString trees="(";
-    for(auto &treename:treenames)
-    {
-        auto chain = new TChain(treename.Data());
-        fChains.push_back(chain);
-        trees+=" "+treename;
-    }
-    trees+=" )";
-    
-    for(auto branch:fBranches)
-    {
-        auto hstack = new THStack(Form("PlotterStack%d",counter),Form("Plotter Stack Branch=%s Trees=%s",branch.Data(),trees.Data()));
-        fHStacks[branch.Data()]=hstack;
-    }
-    fCanvas = new TCanvas(Form("canvas%d",counter));
-    counter++;
+Plotter::Plotter(std::string treename,const std::vector<std::string> branches, UInt_t bins,Double_t xmin, Double_t xmax):fBranches(branches),fNBins(bins),fXmin(xmin),fXmax(xmax),fOutput(nullptr),fTreeName(treename)
+{    
 }
 
 
@@ -44,50 +28,91 @@ Plotter::~Plotter()
     //TODO: free vectors of pointers and map
     if(fCanvas!=nullptr) delete fCanvas;
 }
-    
+
 //______________________________________________________________________________
-void Plotter::AddFile(const char *filename, const char *treename,Long64_t nentries)
+void Plotter::AddDirectory(const char *path,const char *alias,Double_t weight)
 {
+    auto files=Find(path);        //getting paths for files *.root
+    if(!fChains.count(alias))     //creating chain if dont exists
+    {
+        auto chain = new TChain(fTreeName.c_str());   
+        fChains[alias]=chain;
+    }
+    
+    for(auto &file:files)  //filling the chain with the files
+       fChains[alias]->AddFile(file.c_str(),TTree::kMaxEntries,fTreeName.c_str());
+    //NOTE: if you call two times this function changing the weight
+    //all the previous weights will be change for the last one.
+    fChains[alias]->SetWeight(weight);//setting weight 
+}
+
+//______________________________________________________________________________
+void Plotter::AddFile(const char *alias,const char *filename, Double_t weight,Long64_t nentries)
+{
+    if(!fChains.count(alias))     //creating chain if dont exists
+    {
+        auto chain = new TChain(fTreeName.c_str());   
+        fChains[alias]=chain;
+    }
+    fChains[alias]->AddFile(filename,nentries,fTreeName.c_str());
+}
+
+//______________________________________________________________________________
+std::pair<THStack*,TLegend*> &Plotter::GetPlot(const Char_t *branch)
+{
+    std::string aliasnames="( ";
+    std::vector<std::string> aliases;
     for(auto &chain:fChains)
     {
-        chain->AddFile(filename,nentries,treename);
+        auto name=chain.first;
+        aliases.push_back(name);
+        aliasnames+=name;
+        aliasnames+=" ";
     }
-}
+    aliasnames+=")";
     
-//______________________________________________________________________________
-void Plotter::AddFriend(TTree *chainOrTree,const char *alias)
-{
+    if(!fHStacks.count(branch))     //creating chain if dont exists
+    {
+        auto leg = new TLegend(0.68,0.72,0.98,0.92);
+        auto hstack = new THStack(Form("PlotterStack%d",counter),Form("Plotter Stack Branch=%s Aliases=%s",branch,aliasnames.c_str()));
+        fHStacks[branch]=std::pair<THStack*,TLegend*>(hstack,leg);
+    }
+    auto color=2;
     for(auto &chain:fChains)
     {
-        chain->AddFriend(chainOrTree,alias);
-    }
-}
-    
-//______________________________________________________________________________
-std::map<TString,THStack*>  &Plotter::GetPlots()
-{
-    auto color=5;
-    for(auto &branch:fBranches)
-    {
-        auto bname=branch.Data();
-        for(auto &chain:fChains)
-        {
-            auto cname=chain->GetName();
-            if(chain->GetBranch(bname)==nullptr)
+            auto cname=chain.first.c_str();
+            if(chain.second->GetBranch(branch)==nullptr)
             {
-                Error(Form("%s::%s(%d)",__FILE__,__FUNCTION__,__LINE__),"Branch %s not found omitting...",bname);
+                Error(Form("%s::%s(%d)",__FILE__,__FUNCTION__,__LINE__),"Branch %s not found omitting...",branch);
                 continue;//branch dont exists, omitting and continue to the next branch
             }
-            auto h=new TH1F(Form("%s%s",cname,bname),Form("%s%s",cname,bname),fNBins,fXmin,fXmax);
-            chain->Draw(Form("%s>>%s",bname,Form("%s%s",cname,bname)));
+            auto h=new TH1F(Form("%s%s",cname,branch),Form("%s%s",cname,branch),fNBins,fXmin,fXmax);
+            TCut cuts="";
+            if(fCuts.count(cname)) cuts=fCuts[cname];
+            chain.second->Draw(Form("%s>>%s",branch,Form("%s%s",cname,branch)),cuts,"goff");
             h->SetFillColor(color);
+            h->SetLineColor(color);
             color++;
-            fHStacks[bname]->Add(h);
-        }
+            fHStacks[branch].first->Add(h); //adding histogram to THStack
+            fHStacks[branch].second->AddEntry(h,cname,"l"); //Adding legend
     }
-    //fCanvas->Close();
-    return fHStacks;
+    return fHStacks[branch];
 }
+
+//______________________________________________________________________________
+std::map<std::string,std::pair<THStack*,TLegend*> > &Plotter::GetPlots()
+{
+        for(auto &branch:fBranches)
+            GetPlot(branch.c_str());
+        return fHStacks;
+}
+
+//______________________________________________________________________________
+void Plotter::SetCut(const Char_t *alias, TCut cut)
+{
+        fCuts[alias]=cut;
+}
+
 
 //______________________________________________________________________________
 void Plotter::Print()
@@ -95,13 +120,28 @@ void Plotter::Print()
     std::cout<<"---------------- Chains ----------------<<std::endl"<<std::endl;
     for(auto &chain:fChains)
     {
-        chain->Print();
+        std::cout<<chain.first<<std::endl;
+        chain.second->Print();
     }
     std::cout<<"---------------- HStacks ----------------<<std::endl"<<std::endl;
     for(auto &hstack:fHStacks)
     {
-        hstack.second->Print();
+        std::cout<<hstack.first<<std::endl;
+        hstack.second.first->Print();
     }
+}
+
+//______________________________________________________________________________
+std::vector<std::string> Plotter::Find(std::string path, std::string pattern)
+{
+    auto files=gSystem->GetFromPipe(Form("find %s -name \"%s\"",path.c_str(),pattern.c_str()));
+    std::vector<std::string> strings;
+    std::istringstream f(files.Data());
+    std::string s;
+    while (getline(f, s, '\n')) {
+        strings.push_back(s);
+    }
+   return strings;
 }
 
 
